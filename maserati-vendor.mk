ffb5b32c79bfdd2fe0964fe7f9e4c449c8d6ad2a
Hashcode <hashcode0f@gmail.com>
2012-12-05 00:57:31 -0800

    no more MotoSimUI
    Change-Id: I1dd7d349fd9c22418b83ef1ef069bda23e131325

--- maserati-vendor.mk
+++ maserati-vendor.mk
@@ -19,5 +19,5 @@ $(call inherit-product, vendor/motorola/maserati/maserati-vendor-blobs.mk)
 $(call inherit-product, vendor/motorola/common/common-vendor.mk)
 
 # MotoSimUI
-PRODUCT_PACKAGES += MotoSimUiHelper
+#PRODUCT_PACKAGES += MotoSimUiHelper
 
