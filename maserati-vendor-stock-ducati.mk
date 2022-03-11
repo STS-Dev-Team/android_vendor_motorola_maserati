73ba72cef5b9e73e9ef3263ed2e74c3a0945eb69
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-11 22:48:38 +0800

    Delete maserati-vendor-stock-ducati.mk

--- maserati-vendor-stock-ducati.mk
+++
@@ -1,6 +0,0 @@
-VENDOR_BLOB_FOLDER := vendor/motorola/maserati/proprietary
-
-# system/etc
-PRODUCT_COPY_FILES += \
-$(VENDOR_BLOB_FOLDER)/etc/firmware/ducati-m3.bin:/system/etc/firmware/ducati-m3.bin
-
